4ccfb77609d687f6498d300ef3396ca25d21b761
mehdisayn <m21gits@gmail.com>
2025-08-01 15:42:33 +0600

    Factory manager FXML

--- src/main/java/devsharkit/papermill/Controller/factoryManager/MaterialApproval.java
+++ src/main/java/devsharkit/papermill/Controller/factoryManager/MaterialApproval.java
@@ -1,7 +1,35 @@
 package devsharkit.papermill.Controller.factoryManager;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class MaterialApproval
 {
     @javafx.fxml.FXML
+    private TableColumn FMmaterialApprovalMaterialColumn;
+    @javafx.fxml.FXML
+    private TableColumn FMmaterialApprovalCostColumn;
+    @javafx.fxml.FXML
+    private TableView FMmaterialApprovalTableView;
+    @javafx.fxml.FXML
+    private TableColumn FMmaterialApprovalQuantityColumn;
+    @javafx.fxml.FXML
+    private TableColumn FMmaterialApprovalReqIDColumn;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void FMmaterialApprovalApproveOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void FMmaterialApprovalViewStockOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void FMmaterialApprovalRejectOnAction(ActionEvent actionEvent) {
+    }
+}
