00645f7b7bdb556cb0a996d9cb1340ce27960857
mehdisayn <m21gits@gmail.com>
2025-08-01 21:05:12 +0600

    Ready FXML CEO

--- src/main/java/devsharkit/papermill/Controller/ceo/CeoDashboard.java
+++ src/main/java/devsharkit/papermill/Controller/ceo/CeoDashboard.java
@@ -1,7 +1,46 @@
 package devsharkit.papermill.Controller.ceo;
 
+import javafx.event.ActionEvent;
+
 public class CeoDashboard
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void setPoliciesCEOOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void strategyCEOOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void productionMonitorCEOOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void complianceCEOOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void expenditureApprovalCEOOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void signoutCEOOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void financialReportCEOOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void DeptPerfCEOOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void boardMeetingsCEOOnAction(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/devsharkit/papermill/Controller/ceo/StrategyPanel.java
+++ src/main/java/devsharkit/papermill/Controller/ceo/StrategyPanel.java
@@ -1,7 +1,33 @@
 package devsharkit.papermill.Controller.ceo;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+
 public class StrategyPanel
 {
     @javafx.fxml.FXML
+    private TableColumn deptColumn;
+    @javafx.fxml.FXML
+    private TextArea txtStrategyNotes;
+    @javafx.fxml.FXML
+    private TableColumn summaryColumn;
+    @javafx.fxml.FXML
+    private ComboBox comboDepartment;
+    @javafx.fxml.FXML
+    private ComboBox comboQuarter;
+    @javafx.fxml.FXML
+    private TableColumn dateSavedColumn;
+    @javafx.fxml.FXML
+    private TableView tableStrategyRecords;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void saveStrategOnAction(ActionEvent actionEvent) {
+    }
+}
